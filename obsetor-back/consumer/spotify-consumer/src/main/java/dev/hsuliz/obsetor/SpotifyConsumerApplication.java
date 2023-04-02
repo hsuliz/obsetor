47283b4fb4eed7a654c5d0729a628fb8d33fc8c0
mymytre <sasha.suliz@gmail.com>
2023-04-02 15:13:15 +0200

    init

---
+++ obsetor-back/consumer/spotify-consumer/src/main/java/dev/hsuliz/obsetor/SpotifyConsumerApplication.java
@@ -0,0 +1,12 @@
+package dev.hsuliz.obsetor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyConsumerApplication.class, args);
+    }
+}
