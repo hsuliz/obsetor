8aef5109365fd694d0c7e40fe4392c1f81fbb905
mymytre <sasha.suliz@gmail.com>
2023-04-25 16:00:10 +0200

    gateway -> add link to auth

--- obsetor-back/gateway/src/main/java/dev/hsuliz/obsetor/GatewayApplication.java
+++ obsetor-back/gateway/src/main/java/dev/hsuliz/obsetor/GatewayApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpotifyConsumerApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpotifyConsumerApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
+
 }
